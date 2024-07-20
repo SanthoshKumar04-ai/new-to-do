ac54a33a2f794e15f47340b60b5f7c4e7cbdddde
SanthoshKumar04-ai <143688286+SanthoshKumar04-ai@users.noreply.github.com>
2024-07-20 16:13:29 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('List');
+
+    if (taskInput.value === '') {
+        alert('Please enter a task to add');
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.textContent = taskInput.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'DELETE';
+    deleteButton.onclick = function() {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+}
